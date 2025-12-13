7eb34ddf6efa22d8a0ba85ac5cbc881f57c0c785
Alex <vanyazvonkov@gmail.com>
2025-12-13 20:36:23 +0300

    Fix bool operator

--- homework-14-12-25/chocolate.py
+++ homework-14-12-25/chocolate.py
@@ -4,7 +4,7 @@ sizes = user_input.split(',')
 
 width, length, size = sizes
 
-can_be_sliced = (int(size) % int(width) == 0) or int(size) // int(width) 
+can_be_sliced = (int(size) % int(width) == 0) and int(size) // int(width) 
 
 print(f'Can be sliced: {can_be_sliced}')
 
